33a026e8ec08230791789cafb4d929531cfaeb96
Roice Johnson <rjohn230uncc.edu>
2019-05-14 20:20:55 -0400

    navbar bugg fixed just need ngbootstrap

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,16 +4,21 @@ import { LayoutRoutingModule } from './layout.routing.module';
 import { CommonModule } from '@angular/common';
 import { GracenavbarComponent } from '../shared/gracenavbar/gracenavbar.component';
 import { NavbarComponent } from 'app/shared/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
     declarations: [
         LayoutComponent,
         GracenavbarComponent,
+
     ],
     imports: [
         CommonModule,
-        LayoutRoutingModule
+        LayoutRoutingModule,
+        NgbModule,
+        
     ]
 })
 export class LayoutModule { }
