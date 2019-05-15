72deaae242f0b9f731bc6eb074d0109f2a634af8
Roice Johnson <rjohn230uncc.edu>
2019-05-14 21:15:44 -0400

    not much

---
+++ src/app/shared/common/commonly.shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    
+  ],
+  exports: [
+    
+  ]
+})
+
+export class CommonlySharedModule { }
